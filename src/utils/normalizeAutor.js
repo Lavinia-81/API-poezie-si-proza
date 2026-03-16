def3ad435ecefcdd6a8579423a10db0ac80aea8b
Lavinia <marialavinia801@yahoo.com>
2026-03-16 12:48:07 +0000

    new commit

--- src/utils/normalizeAutor.js
+++ src/utils/normalizeAutor.js
@@ -1,5 +1,6 @@
 export function normalizeAutor(autor) {
     return autor
+        .normalize("NFC")
         .normalize("NFD")                 // separă diacriticele
         .replace(/[\u0300-\u036f]/g, "")  // elimină diacriticele
         .replace(/\.txt$/i, "")
@@ -7,4 +8,13 @@ export function normalizeAutor(autor) {
         .replace(/\s+/g, " ")
         .trim()
         .toLowerCase();
+}
+
+
+export function normalizeTitlu(titlu) {
+  return decodeURIComponent(titlu)
+    .normalize("NFC")
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, " ");
 }
